2877f7219f4425eedeb48a292b63da2375fc713c
Icymuja <abdulmujahid101@gmail.com>
2025-01-27 10:39:47 +0530

    initial commit

---
+++ src/defaulta/Test1.java
@@ -0,0 +1,14 @@
+package defaulta;
+
+public class Test1 {
+public static void main(String[] args) {
+	String name="muajid";
+	if (name.contains("m")) {
+		System.out.println("this name start with m");}
+		else {
+			System.out.println("name does not start with m");
+		}
+
+		 }
+}
+
